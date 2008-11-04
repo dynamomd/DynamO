183d5498c7cead769b7d37db6e1a01d34a8ecdd2
Marcus Bannerman <mjki2mb2@mjki2mb2-desktop.(none)>
2008-11-04 20:30:48 +0000

    Started the orientation liouvillean for Robert

---
+++ src/dynamics/liouvillean/OrientationL.hpp
@@ -0,0 +1,49 @@
+/*  DYNAMO:- Event driven molecular dynamics simulator 
+    http://www.marcusbannerman.co.uk/dynamo
+    Copyright (C) 2008  Marcus N Campbell Bannerman <m.bannerman@gmail.com>
+
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    version 3 as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CLNewton_H
+#define CLNewton_H
+
+#include "NewtonL.hpp"
+#include <vector>
+#include "../../datatypes/vector.hpp"
+
+class CLNOrientation: public CLNewton
+{
+public:
+  CLNOrientation(DYNAMO::SimData* Sim):
+    CLNewton(Sim)
+  {}
+
+  virtual CLiouvillean* Clone() const { return new CLNOrientation(*this); }
+
+  //Remember to update the signatures in the Liouvillean base clas when you change these
+  virtual Iflt getLineLineCollision() const;
+  
+  //Remember to update the signatures in the Liouvillean base clas when you change these
+  virtual C2ParticleData runLineLineCollision() const;
+
+protected:
+  virtual void outputXML(xmlw::XmlStream&) const;
+
+  virtual void streamParticle(CParticle&, const Iflt&) const;
+
+  typedef std::pair<CVector<>, CVector<> > rotationPair;
+  
+  std::vector<rotationPair> orientationData;
+};
+#endif
