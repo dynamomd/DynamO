35cda84b746d25dc7bca675dae23d600023107bc
Marcus Bannerman <m.bannerman@gmail.com>
2008-11-09 16:08:38 +0000

    Added the MSD correlator for diffusion properties

---
+++ src/outputplugins/tickerproperty/msdcorrelator.cpp
@@ -0,0 +1,150 @@
+/*  DYNAMO:- Event driven molecular dynamics simulator 
+    http://www.marcusbannerman.co.uk/dynamo
+    Copyright (C) 2008  Marcus N Campbell Bannerman <m.bannerman@gmail.com>
+
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    version 3 as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "msdcorrelator.hpp"
+#include <boost/foreach.hpp>
+#include "../../extcode/xmlwriter.hpp"
+#include "../../dynamics/include.hpp"
+#include "../../base/is_simdata.hpp"
+#include "../../dynamics/liouvillean/liouvillean.hpp"
+#include "../0partproperty/msd.hpp"
+#include "../../extcode/mathtemplates.hpp"
+#include "../../dynamics/systems/sysTicker.hpp"
+
+COPMSDCorrelator::COPMSDCorrelator(const DYNAMO::SimData* tmp, const XMLNode&):
+  COPTicker(tmp,"PeriodicMSD"),
+  length(20),
+  currCorrLength(0),
+  ticksTaken(0),
+  notReady(true)
+{}
+
+void 
+COPMSDCorrelator::initialise()
+{
+  posHistory.resize(Sim->lN, boost::circular_buffer<CVector<> >(length));
+
+  currCorrLength=1;
+
+  BOOST_FOREACH(const CParticle& part, Sim->vParticleList)
+    posHistory[part.getID()].push_front(part.getPosition());
+
+  speciesData.resize(Sim->Dynamics.getSpecies().size(), std::vector<Iflt>(length, 0.0));
+
+  structData.resize(Sim->Dynamics.getTopology().size(), std::vector<Iflt>(length, 0.0));
+}
+
+void 
+COPMSDCorrelator::ticker()
+{
+  BOOST_FOREACH(const CParticle& part, Sim->vParticleList)
+    posHistory[part.getID()].push_front(part.getPosition());
+  
+  if (notReady)
+    {
+      if (++currCorrLength != length)
+	return;
+      
+      notReady = false;
+    }
+  
+  accPass();
+}
+
+void
+COPMSDCorrelator::accPass()
+{
+  ++ticksTaken;
+  
+  BOOST_FOREACH(const CSpecies& sp, Sim->Dynamics.getSpecies())
+    BOOST_FOREACH(const size_t& ID, *sp.getRange())
+    for (size_t step(1); step < length; ++step)
+      speciesData[sp.getID()][step] += (posHistory[ID][step] - posHistory[ID][0]).square();
+  
+  BOOST_FOREACH(const smrtPlugPtr<CTopology>& topo, Sim->Dynamics.getTopology())
+    BOOST_FOREACH(const smrtPlugPtr<CRange>& range, topo->getMolecules())
+    {
+      CVector<> molCOM(0);
+      Iflt molMass(0);
+
+      BOOST_FOREACH(const size_t& ID, *range)
+	{
+	  molCOM += posHistory[ID][0] * Sim->Dynamics.getSpecies(Sim->vParticleList[ID]).getMass();
+	  molMass += Sim->Dynamics.getSpecies(Sim->vParticleList[ID]).getMass();
+	}
+
+      molCOM /= molMass;
+
+      for (size_t step(1); step < length; ++step)
+	{
+	  CVector<> molCOM2(0);
+
+	  BOOST_FOREACH(const size_t& ID, *range)
+	    molCOM2 += posHistory[ID][step] * Sim->Dynamics.getSpecies(Sim->vParticleList[ID]).getMass();
+	  
+	  molCOM2 /= molMass;
+	  
+	  structData[topo->getID()][step] += (molCOM2 - molCOM).square();
+	}
+    }
+}
+
+void
+COPMSDCorrelator::output(xmlw::XmlStream &XML)
+{
+  XML << xmlw::tag("MSDCorrelator") 
+      << xmlw::tag("Particles");
+
+  Iflt dt = dynamic_cast<const CSTicker&>(*Sim->Dynamics.getSystem("SystemTicker")).getPeriod()
+    / Sim->Dynamics.units().unitTime();
+
+  BOOST_FOREACH(const CSpecies& sp, Sim->Dynamics.getSpecies())
+    {
+      XML << xmlw::tag("Species")
+	  << xmlw::attr("Name")
+	  << sp.getName()
+	  << xmlw::chardata();
+      
+      for (size_t step(0); step < length; ++step)
+	XML << dt * step << " "
+	    << speciesData[sp.getID()][step] / (ticksTaken * sp.getCount())
+	    << "\n";
+	
+      XML << xmlw::endtag("Species");
+    }
+
+  XML << xmlw::endtag("Particles")
+      << xmlw::tag("Topology");
+  
+  BOOST_FOREACH(const smrtPlugPtr<CTopology>& topo, Sim->Dynamics.getTopology())
+    {
+      XML << xmlw::tag("Structure")
+	  << xmlw::attr("Name")
+	  << topo->getName()
+	  << xmlw::chardata();
+      
+      for (size_t step(0); step < length; ++step)
+	XML << dt * step << " "
+	    << structData[topo->getID()][step] / (ticksTaken * topo->getMolecules().size())
+	    << "\n";
+	
+      XML << xmlw::endtag("Structure");
+    }
+
+  XML << xmlw::endtag("Topology")
+      << xmlw::endtag("MSDCorrelator");
+}
---
+++ src/outputplugins/tickerproperty/msdcorrelator.hpp
@@ -0,0 +1,52 @@
+/*  DYNAMO:- Event driven molecular dynamics simulator 
+    http://www.marcusbannerman.co.uk/dynamo
+    Copyright (C) 2008  Marcus N Campbell Bannerman <m.bannerman@gmail.com>
+
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    version 3 as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef COPMSDCorrelator_H
+#define COPMSDCorrelator_H
+
+#include "ticker.hpp"
+#include <boost/circular_buffer.hpp>
+
+class COPMSD;
+
+class COPMSDCorrelator: public COPTicker
+{
+ public:
+  COPMSDCorrelator(const DYNAMO::SimData*, const XMLNode&);
+
+  virtual void initialise();
+
+  void output(xmlw::XmlStream &); 
+
+  virtual COutputPlugin *Clone() const { return new COPMSDCorrelator(*this); };
+  
+ protected:
+  virtual void stream(Iflt) {}
+  virtual void ticker();
+
+  void accPass();
+
+  std::vector<boost::circular_buffer<CVector<> > > posHistory;
+  std::vector<std::vector<Iflt> > speciesData;
+  std::vector<std::vector<Iflt> > structData;
+  size_t length;
+  size_t currCorrLength;
+  size_t ticksTaken;
+  bool notReady;
+};
+
+#endif
