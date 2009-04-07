764133304c981ed2263dfdd4bd8c3f85b1f460a6
Marcus Bannerman <mjki2mb2@mjki2mb2-desktop.(none)>
2009-04-07 07:01:47 +0100

    Some script updates

--- test/profile.sh
+++ test/profile.sh
@@ -1,17 +1,43 @@
-#>profile.results
+#/bin/bash
 
-for FCC in $(seq 51 60);do
-    for DENS in 0.5; do
-	./configmod -i2 -C $FCC -d $DENS > /dev/null
+NCOLL=20000000
+
+for FCC in $(seq 4 10) $(seq 12 2 20) $(seq 24 4 40) $(seq 45 5 70) $(seq 80 10 100);do
+    for DENS in 0.2 0.5 0.8 ; do
+	dynamod -m0 -C $FCC -d $DENS > /dev/null
 	
-	CellCount=$(./mdrun config.out.xml.bz2 -c 1000000 | grep Cells | gawk '{print $4}' | gawk  -F',' '{print $1}')
+	CellCount=$(dynarun config.out.xml.bz2 -c $NCOLL | grep "Cells <x,y,z>" | gawk '{print $4}' | gawk  -F',' '{print $1}')
 	
-	t=$(bzcat output.xml.bz2 | xml sel -t -v '/OutputData/Misc/Timing/CollPerSec/@val')
-	
+	t1=$(bzcat output.xml.bz2 | xmlstarlet sel -t -v '/OutputData/Misc/Timing/CollPerSec/@val')
+	dynarun config.out.xml.bz2 -c $NCOLL > /dev/null
+	t2=$(bzcat output.xml.bz2 | xmlstarlet sel -t -v '/OutputData/Misc/Timing/CollPerSec/@val')
+	dynarun config.out.xml.bz2 -c $NCOLL > /dev/null
+	t3=$(bzcat output.xml.bz2 | xmlstarlet sel -t -v '/OutputData/Misc/Timing/CollPerSec/@val')
+
 	N=$(bzcat output.xml.bz2 | grep -i ParticleCount | gawk -F'"' '{print $2}')
 	celldens=$(gawk 'BEGIN {print '$N'/(('$CellCount')^3)}')
 
-	echo "$N $celldens $t $CellCount" >> profile.results
-	echo "$N $celldens $t $CellCount"
+	echo "$N $celldens $CellCount $t1 $t2 $t3" >> profile.Rho$DENS.results
+	echo "$N $celldens $CellCount $t1 $t2 $t3"
+    done
+done
+
+for FCC in 20 25 30 ;do
+    for DENS in $(seq 0.05 0.05 1.2) ; do
+	dynamod -m0 -C $FCC -d $DENS > /dev/null
+	
+	CellCount=$(dynarun config.out.xml.bz2 -c $NCOLL | grep "Cells <x,y,z>" | gawk '{print $4}' | gawk  -F',' '{print $1}')
+	
+	t1=$(bzcat output.xml.bz2 | xmlstarlet sel -t -v '/OutputData/Misc/Timing/CollPerSec/@val')
+	dynarun config.out.xml.bz2 -c $NCOLL > /dev/null
+	t2=$(bzcat output.xml.bz2 | xmlstarlet sel -t -v '/OutputData/Misc/Timing/CollPerSec/@val')
+	dynarun config.out.xml.bz2 -c $NCOLL > /dev/null
+	t3=$(bzcat output.xml.bz2 | xmlstarlet sel -t -v '/OutputData/Misc/Timing/CollPerSec/@val')
+
+	N=$(bzcat output.xml.bz2 | grep -i ParticleCount | gawk -F'"' '{print $2}')
+	celldens=$(gawk 'BEGIN {print '$N'/(('$CellCount')^3)}')
+
+	echo "$N $celldens $CellCount $t1 $t2 $t3" >> profile.F$FCC.results
+	echo "$N $celldens $CellCount $t1 $t2 $t3"
     done
 done
