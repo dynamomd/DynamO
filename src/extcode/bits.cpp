e4c8799e050d4734030ab85873c029111a100e19
Marcus Bannerman <mjki2mb2@mjki2mb2-desktop.(none)>
2009-04-20 20:51:27 +0100

    Completed a dilated integer class

--- src/extcode/bits.cpp
+++ src/extcode/bits.cpp
@@ -2,69 +2,91 @@
 #include <limits>
 #include <assert.h>
 
-//bit masks for the dimensions
-const unsigned int dimbits[] = {0x49249249, 0x92492492, 0x24924924};
+template<typename T,
+	 T mask, 
+	 T dimension,
+	 T S = 10,
+	 T Smask = 0x03FF>
+class MaskedInteger{
+public:
+    // Constructor and Getter
+  MaskedInteger(const T& val): 
+    value(dilate_3(val & Smask) << dimension) {}
 
-inline unsigned int incrementMorton(const unsigned int& MortonNum, const unsigned int& mask)
+  inline const T& getDilatedVal() { return value; }
+
+  inline T getRealVal() { return undilate_3(value >> dimension); }
+
+  // Simple operators.
+  MaskedInteger operator-(const MaskedInteger& d) const
+  { return MaskedInteger((value - d.value) & mask); }
+  MaskedInteger operator+(const MaskedInteger& d) const
+  { return MaskedInteger((value + (~mask) + d.value) & mask); }
+  inline void operator++() { value = (value - mask) & mask; }
+  inline void operator--() { value = (value -    1) & mask; }
+  inline void operator==(const MaskedInteger& d) const
+  { return value == d.value; }
+  inline void operator!=(const MaskedInteger& d) const
+  { return value != d.value; }
+  inline void operator<(const MaskedInteger& d) const
+  { return value < d.value; }
+  inline void operator>(const MaskedInteger& d) const
+  { return value > d.value; }
+  inline void operator<=(const MaskedInteger& d) const
+  { return value <= d.value; }
+  inline void operator>=(const MaskedInteger& d) const
+  { return value >= d.value; }
+
+private:
+  T value; // stored as normalized integer at mask’s  1 bits.
+
+  inline unsigned short undilate_3(unsigned int t){
+    t = (t * 0x00015) & 0x0E070381;
+    t = (t * 0x01041) & 0x0FF80001;
+    t = (t * 0x40001) & 0x0FFC0000;
+    return ((unsigned short) (t >> 18));
+  }
+
+  inline unsigned int dilate_3(unsigned short t) {
+    unsigned int r = t;
+    r =  (r * 0x10001) & 0xFF0000FF; 
+    r =  (r * 0x00101) & 0x0F00F00F;
+    r =  (r * 0x00011) & 0xC30C30C3;
+    r =  (r * 0x00005) & 0x49249249;
+    return(r);
+  }
+
+};
+
+typedef MaskedInteger<unsigned int, 0x49249249, 0> MI0;
+typedef MaskedInteger<unsigned int, 0x92492492, 1> MI1;
+typedef MaskedInteger<unsigned int, 0x24924924, 2> MI2;
+
+struct dilatedCoords 
 {
-  return (MortonNum & (dimbits[mask] ^ 0xFFFFFFFF))
-    | ((MortonNum & dimbits[mask]) - dimbits[mask]) & dimbits[mask];
-}
+  dilatedCoords(const unsigned short& x, const unsigned short& y, const unsigned short& z):
+    _x(x), _y(y), _z(z)
+  {}
 
-inline unsigned int decrementMorton(const unsigned int& MortonNum, const unsigned int& mask)
-{
-  return (MortonNum & (dimbits[mask] ^ 0xFFFFFFFF))
-    | ((MortonNum & dimbits[mask]) - 1) & dimbits[mask];
-}
+  inline unsigned int getMortonNum()
+  { return _x.getDilatedVal() + _y.getDilatedVal() + _z.getDilatedVal(); }
 
-/*inline unsigned int addMorton(const unsigned int& MortonNum, const unsigned int& add,
-			      const unsigned int& mask)
-{
-  return (MortonNum & (dimbits[mask] ^ 0xFFFFFFFF))
-    | ((MortonNum & dimbits[mask]) - 1) & dimbits[mask];
-    }*/
-
-unsigned int get3DMortonNum(const unsigned char& x,
-			    const unsigned char& y,
-			    const unsigned char& z)
-{
-  assert(sizeof(unsigned int) == 4);
-  unsigned int coord(0);
-  
-  for (int i = 0; i < sizeof(x) * CHAR_BIT; i++)
-    {
-      coord |=
-	((x & (1 << i)) << (i*2))
-	| ((y & (1 << i)) << (i*2 + 1))
-	| ((z & (1 << i)) << (i*2 + 2));
-    }
-
-  return coord;
-}
-
-void get3DMortonCoords(const unsigned int& MortonNum)
-{
-  assert(sizeof(unsigned int) == 4);
- 
-  unsigned int x(0), y(0), z(0);
-
-  for (int i = 0; i < sizeof(unsigned char) * CHAR_BIT; i++)
-    {
-      x |= (MortonNum & (1 << i*3)) >> (i*2);
-      y |= (MortonNum & (1 << i*3+1)) >> (i*2+1);
-      z |= (MortonNum & (1 << i*3+2)) >> (i*2+2);
-    }
-
-  std::cout //<< std::hex << std::showbase 
-	    << x << " " << y << " " << z << std::endl;
-}
+  MI0 _x;
+  MI1 _y;
+  MI2 _z;
+};
 
 int main()
 {
-  unsigned int MortonNum = get3DMortonNum(2,0,3);
-  get3DMortonCoords(MortonNum);
-  get3DMortonCoords(decrementMorton(MortonNum,0));
-  get3DMortonCoords(decrementMorton(MortonNum,1));
-  get3DMortonCoords(decrementMorton(MortonNum,2));
+  dilatedCoords test(1023,1023,1023);
+  
+  ++test._x;
+  std::cout << test._x.getRealVal() << " " << test._y.getRealVal() << " " << test._z.getRealVal() <<  std::endl;
+
+  ++test._y;
+  std::cout << test._x.getRealVal() << " " << test._y.getRealVal() << " " << test._z.getRealVal() <<  std::endl;
+
+  ++test._z;
+  std::cout << test._x.getRealVal() << " " << test._y.getRealVal() << " " << test._z.getRealVal() <<  std::endl;
   
 }
