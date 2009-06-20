06253a67a8df96733f2d070a6a6307594264be23
Marcus <mjki2mb2@mjki2mb2-desktop.(none)>
2009-06-20 10:47:02 +0100

    It still won't run

--- LJsteps/makeLJconfig.sh
+++ LJsteps/makeLJconfig.sh
@@ -8,6 +8,6 @@ bzcat config.out.xml.bz2 | xml sel -t -v '//ParticleData/@N' >> config.gro
 bzcat config.out.xml.bz2 \
     | xml sel -t -m '//Pt' -v '@ID' -o ' ' -v 'P/@x' -o ' ' -v 'P/@y' -o ' ' \
     -v 'P/@z' -o ' ' -v 'V/@x' -o ' ' -v 'V/@y' -o ' ' -v 'V/@z' -n \
-    | gawk '{if (NF) print ($1+1)"Ar", "Ar1", $1+1, $2+('$boxlength'/2.0),$3+('$boxlength'/2.0),$4+('$boxlength'/2.0),$5+('$boxlength'/2.0),$6+('$boxlength'/2.0),$7+('$boxlength'/2.0)}' \
+    | gawk '{if (NF) printf "%d%s %s %d %.5f %.5f %.5f %.5f %.5f %.5f\n",($1+1),"Ar", "Ar1", $1+1, $2+('$boxlength'/2.0),$3+('$boxlength'/2.0),$4+('$boxlength'/2.0),$5+('$boxlength'/2.0),$6+('$boxlength'/2.0),$7+('$boxlength'/2.0)}' \
   >> config.gro
 echo "$boxlength $boxlength $boxlength" | gawk '{print $1+0,$2+0,$3+0}' >> config.gro
