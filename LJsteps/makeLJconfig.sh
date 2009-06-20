beff724a02a442cce5b752b79e5b739b82c4544f
Marcus <mjki2mb2@mjki2mb2-desktop.(none)>
2009-06-20 10:55:12 +0100

    Made it output in the gromacs fixed file format

--- LJsteps/makeLJconfig.sh
+++ LJsteps/makeLJconfig.sh
@@ -3,11 +3,15 @@
 boxlength=$(bzcat config.out.xml.bz2 | xml sel -t -v '//Units/@BoxLength')
 
 
-echo "Lennard Jones config " > config.gro
-bzcat config.out.xml.bz2 | xml sel -t -v '//ParticleData/@N' >> config.gro
+echo "Lennard Jones config" > config.gro
+
+bzcat config.out.xml.bz2 | xml sel -t -v '//ParticleData/@N' \
+    | gawk '{printf "%5d\n",$1}' \
+    >> config.gro
+
 bzcat config.out.xml.bz2 \
     | xml sel -t -m '//Pt' -v '@ID' -o ' ' -v 'P/@x' -o ' ' -v 'P/@y' -o ' ' \
     -v 'P/@z' -o ' ' -v 'V/@x' -o ' ' -v 'V/@y' -o ' ' -v 'V/@z' -n \
-    | gawk '{if (NF) printf "%d%s %s %d %.5f %.5f %.5f %.5f %.5f %.5f\n",($1+1),"Ar", "Ar1", $1+1, $2+('$boxlength'/2.0),$3+('$boxlength'/2.0),$4+('$boxlength'/2.0),$5+('$boxlength'/2.0),$6+('$boxlength'/2.0),$7+('$boxlength'/2.0)}' \
+    | gawk '{if (NF) printf "%5d%5s%5s%5d%8.3f%8.3f%8.3f%8.3f%8.3f%8.3f\n",($1+1),"Ar", "Ar1", $1+1, $2+('$boxlength'/2.0),$3+('$boxlength'/2.0),$4+('$boxlength'/2.0),$5+('$boxlength'/2.0),$6+('$boxlength'/2.0),$7+('$boxlength'/2.0)}' \
   >> config.gro
-echo "$boxlength $boxlength $boxlength" | gawk '{print $1+0,$2+0,$3+0}' >> config.gro
+echo $boxlength | gawk '{printf "%10.5f%10.5f%10.5f\n",$1+0,$1+0,$1+0}' >> config.gro
