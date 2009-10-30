1ba91f9c9fd07fa50e7b8f6b8d80702abee017ae
Robert Sargant <robert.sargant@postgrad.manchester.ac.uk>
2009-10-30 16:47:09 +0000

    SelfDiffusionOrientationGK compiles OK; probably numerically wrong pending testing

--- src/outputplugins/correlations/selfdiffOrientationalGK.cpp
+++ src/outputplugins/correlations/selfdiffOrientationalGK.cpp
@@ -44,20 +44,19 @@ OPSelfDiffusionOrientationalGK::initialise()
 		D_throw() << "Species does not specify an orientation";
   }
 
-  G_parallel.resize(Sim->lN, boost::circular_buffer<Vector  >(CorrelatorLength, Vector(0,0,0)));
-  G_perp.resize(Sim->lN, boost::circular_buffer<Vector  >(CorrelatorLength, Vector(0,0,0)));
+  G.resize(Sim->lN, boost::circular_buffer<VUpair> (CorrelatorLength, VUpair(Vector(0,0,0), Vector(0,0,0) )));
 
   accG2_parallel.resize(Sim->dynamics.getSpecies().size());
   accG2_perp.resize(Sim->dynamics.getSpecies().size());
 
-  BOOST_FOREACH(std::vector<Vector  >& listref_parallel, accG2_parallel)
+  BOOST_FOREACH(std::vector<Iflt>& listref_parallel, accG2_parallel)
   {
-    listref_parallel.resize(CorrelatorLength, Vector (0,0,0));
+    listref_parallel.resize(CorrelatorLength, 0);
   }
 
-  BOOST_FOREACH(std::vector<Vector  >& listref_perp, accG2_perp)
+  BOOST_FOREACH(std::vector<Iflt>& listref_perp, accG2_perp)
   {
-    listref_perp.resize(CorrelatorLength, Vector (0,0,0));
+    listref_perp.resize(CorrelatorLength, 0);
   }
 
   I_cout() << "dt set to " << dt / Sim->dynamics.units().unitTime();
@@ -159,13 +158,13 @@ OPSelfDiffusionOrientationalGK::newG(const C1ParticleData& PDat)
 
 	for (size_t i = 0; i < Sim->lN; ++i)
 	{
-		G_perp[i].push_front(Sim->vParticleList[i].getVelocity());
-		G_parallel[i].push_front(Sim->vParticleList[i].getVelocity());
+		const LNOrientation::rotData& rdat(static_cast<const LNOrientation&> (Sim->dynamics.getLiouvillean()).getRotData(Sim->vParticleList[i]));
+		G[i].push_front(VUpair(Sim->vParticleList[i].getVelocity(), rdat.orientation));
 	}
 
-	//Now correct the fact that the wrong velocity has been pushed
-	G_perp[PDat.getParticle().getID()].front() = PDat.getOldVel();
-	G_parallel[PDat.getParticle().getID()].front() = PDat.getOldVel();
+	//Now correct the fact that the wrong velocity and orientation have been pushed
+	const LNOrientation::rotData& fetchpart(static_cast<const LNOrientation&> (Sim->dynamics.getLiouvillean()).getRotData(Sim->vParticleList[PDat.getParticle().getID()]));
+	G[PDat.getParticle().getID()].front() = VUpair(PDat.getOldVel(), fetchpart.orientation);
 
 	//This ensures the list gets to accumilator size
 	if (notReady)
@@ -186,16 +185,16 @@ OPSelfDiffusionOrientationalGK::newG(const C2ParticleData& PDat)
 {
 	for (size_t i = 0; i < Sim->lN; ++i)
 	{
-		G_perp[i].push_front(Sim->vParticleList[i].getVelocity());
-		G_parallel[i].push_front(Sim->vParticleList[i].getVelocity());
+		const LNOrientation::rotData& rdat(static_cast<const LNOrientation&> (Sim->dynamics.getLiouvillean()).getRotData(Sim->vParticleList[i]));
+		G[i].push_front(VUpair(Sim->vParticleList[i].getVelocity(), rdat.orientation));
 	}
 
-	//Now correct the fact that the wrong velocity has been pushed
-	G_perp[PDat.particle1_.getParticle().getID()].front() = PDat.particle1_.getOldVel();
-	G_perp[PDat.particle2_.getParticle().getID()].front() = PDat.particle2_.getOldVel();
+	//Now correct the fact that the wrong velocities and orientations have been pushed
+	const LNOrientation::rotData& fetch1(static_cast<const LNOrientation&> (Sim->dynamics.getLiouvillean()).getRotData(Sim->vParticleList[PDat.particle1_.getParticle().getID()]));
+	const LNOrientation::rotData& fetch2(static_cast<const LNOrientation&> (Sim->dynamics.getLiouvillean()).getRotData(Sim->vParticleList[PDat.particle2_.getParticle().getID()]));
 
-	G_parallel[PDat.particle1_.getParticle().getID()].front() = PDat.particle1_.getOldVel();
-	G_parallel[PDat.particle2_.getParticle().getID()].front() = PDat.particle2_.getOldVel();
+	G[PDat.particle1_.getParticle().getID()].front() = VUpair(PDat.particle1_.getOldVel(), fetch1.orientation);
+	G[PDat.particle2_.getParticle().getID()].front() = VUpair(PDat.particle2_.getOldVel(), fetch2.orientation);
 
 	//This ensures the list gets to accumilator size
 	if (notReady)
@@ -217,24 +216,24 @@ OPSelfDiffusionOrientationalGK::newG(const CNParticleData& PDat)
 	//This ensures the list stays at accumilator size
 	for (size_t i = 0; i < Sim->lN; ++i)
 	{
-		G_perp[i].push_front(Sim->vParticleList[i].getVelocity());
-		G_parallel[i].push_front(Sim->vParticleList[i].getVelocity());
+		const LNOrientation::rotData& rdat(static_cast<const LNOrientation&> (Sim->dynamics.getLiouvillean()).getRotData(Sim->vParticleList[i]));
+		G[i].push_front(VUpair(Sim->vParticleList[i].getVelocity(), rdat.orientation));
 	}
 
 	//Go back and fix the pushes
 	BOOST_FOREACH(const C1ParticleData&PDat2, PDat.L1partChanges)
 	{
-		G_perp[PDat2.getParticle().getID()].front() = PDat2.getOldVel();
-		G_parallel[PDat2.getParticle().getID()].front() = PDat2.getOldVel();
+		const LNOrientation::rotData& fetchpart(static_cast<const LNOrientation&> (Sim->dynamics.getLiouvillean()).getRotData(Sim->vParticleList[PDat2.getParticle().getID()]));
+		G[PDat2.getParticle().getID()].front() = VUpair(PDat2.getOldVel(), fetchpart.orientation);
 	}
 
 	BOOST_FOREACH(const C2ParticleData& PDat2, PDat.L2partChanges)
     {
-		G_perp[PDat2.particle1_.getParticle().getID()].front() = PDat2.particle1_.getOldVel();
-		G_perp[PDat2.particle2_.getParticle().getID()].front() = PDat2.particle2_.getOldVel();
+		const LNOrientation::rotData& fetch1(static_cast<const LNOrientation&> (Sim->dynamics.getLiouvillean()).getRotData(Sim->vParticleList[PDat2.particle1_.getParticle().getID()]));
+		const LNOrientation::rotData& fetch2(static_cast<const LNOrientation&> (Sim->dynamics.getLiouvillean()).getRotData(Sim->vParticleList[PDat2.particle2_.getParticle().getID()]));
 
-		G_parallel[PDat2.particle1_.getParticle().getID()].front() = PDat2.particle1_.getOldVel();
-		G_parallel[PDat2.particle2_.getParticle().getID()].front() = PDat2.particle2_.getOldVel();
+		G[PDat2.particle1_.getParticle().getID()].front() = VUpair(PDat2.particle1_.getOldVel(), fetch1.orientation);
+		G[PDat2.particle2_.getParticle().getID()].front() = VUpair(PDat2.particle2_.getOldVel(), fetch2.orientation);
     }
 
 	//This ensures the list gets to accumilator size
@@ -275,7 +274,7 @@ OPSelfDiffusionOrientationalGK::output(xmlw::XmlStream& XML)
 
 
 		// Perpendicular section
-		Vector acc_perp = 0.5*(accG2_perp[i].front() + accG2_perp[i].back());
+		Iflt acc_perp = 0.5*(accG2_perp[i].front() + accG2_perp[i].back());
 
 		for (size_t j = 1; j < accG2_perp[i].size() - 1; ++j)
 			{ acc_perp += accG2_perp[i][j];}
@@ -290,16 +289,14 @@ OPSelfDiffusionOrientationalGK::output(xmlw::XmlStream& XML)
 
 		for (size_t j = 0; j < accG2_perp[i].size(); ++j)
 		{
-			XML << j * dt / Sim->dynamics.units().unitTime();
-			for (size_t iDim = 0; iDim < NDIM; iDim++)
-				XML << "\t" << accG2_perp[i][j][iDim] * factor / specCount;
-			XML << "\n";
+			XML << j * dt / Sim->dynamics.units().unitTime()
+				<< "\t" << accG2_perp[i][j] * factor / specCount << "\n";
 		}
 
 		XML << xmlw::endtag("Component");
 
 		// Parallel section
-		Vector acc_parallel = 0.5*(accG2_parallel[i].front() + accG2_parallel[i].back());
+		Iflt acc_parallel = 0.5*(accG2_parallel[i].front() + accG2_parallel[i].back());
 
 		for (size_t j = 1; j < accG2_parallel[i].size() - 1; ++j)
 		{
@@ -316,10 +313,8 @@ OPSelfDiffusionOrientationalGK::output(xmlw::XmlStream& XML)
 
 		for (size_t j = 0; j < accG2_parallel[i].size(); ++j)
 		{
-			XML << j * dt / Sim->dynamics.units().unitTime();
-			for (size_t iDim = 0; iDim < NDIM; iDim++)
-				XML << "\t" << accG2_parallel[i][j][iDim] * factor / specCount;
-			XML << "\n";
+			XML << j * dt / Sim->dynamics.units().unitTime()
+				<< "\t" << accG2_parallel[i][j] * factor / specCount << "\n";
 		}
 
 		XML << xmlw::endtag("Component")
@@ -359,11 +354,20 @@ OPSelfDiffusionOrientationalGK::accPass()
 		{
 			for (size_t j = 0; j < CorrelatorLength; ++j)
 			{
-				for (size_t iDim(0); iDim < NDIM; ++iDim)
-				{
-					accG2_parallel[spec->getID()][j][iDim] +=  G_parallel[ID].front()[iDim] * G_parallel[ID][j][iDim];
-					accG2_perp[spec->getID()][j][iDim] +=  G_perp[ID].front()[iDim] * G_perp[ID][j][iDim];
-				}
+				// Parallel = <[v(t).u(0)][v(0).u(0)]>
+				accG2_parallel[spec->getID()][j] +=  ((G[ID].front().first |  G[ID][j].second) * (G[ID][j].first | G[ID][j].second));
+
+				// Get a unit matrix
+				Matrix unit, dyad;
+				unit.one();
+				dyad = Dyadic(G[ID][j].second, G[ID][j].second);
+
+				// Perpendicular = <v(t).[I - u(0)u(0)]v(0)>
+				accG2_perp[spec->getID()][j] +=  (G[ID].front().first | ((unit - dyad) * G[ID][j].first));
+
+				//for (size_t iDim(0); iDim < NDIM; ++iDim)
+				//{
+				//}
 			}
 		}
 	}
