0c63204442f70648fdfee23c84282fb8e5ac14ba
Robert Sargant <robert.sargant@postgrad.manchester.ac.uk>
2010-02-28 13:23:19 +0000

    Removed reference to arbitrary window shrinking limit

--- src/dynamics/liouvillean/shapes/frenkelroot.hpp
+++ src/dynamics/liouvillean/shapes/frenkelroot.hpp
@@ -1,4 +1,4 @@
-/*  DYNAMO:- Event driven molecular dynamics simulator 
+/*  DYNAMO:- Event driven molecular dynamics simulator
     http://www.marcusbannerman.co.uk/dynamo
     Copyright (C) 2009  Marcus N Campbell Bannerman <m.bannerman@gmail.com>
 
@@ -20,42 +20,42 @@
 #include "../../../extcode/mathtemplates.hpp"
 
 template<class T>
-Iflt quadRootHunter(const T& fL, Iflt length, Iflt& t_low, Iflt& t_high, 
+Iflt quadRootHunter(const T& fL, Iflt length, Iflt& t_low, Iflt& t_high,
 		    const Iflt& tolerance)
 {
   Iflt working_time = t_low;
   Iflt timescale = tolerance * length / fL.F_firstDeriv_max(length);
   bool fwdWorking = false;
-  
-  size_t w = 0;
+
+  //~ size_t w = 0;
 
   while(t_low < t_high)
     {
       //Always try again from the other side
       fwdWorking = !fwdWorking;
-    
-      if(++w > 1000)
-	{
-	  std::cerr << "\nWindow shrunk thousands of times\n";
-	  std::cerr << "\nt_low = " << t_low << ", t_high = " << t_high 
-		    << ", working_time = " << working_time << "\n";
-	  
-	  return t_low;
-	}
-    
+
+      //~ if(++w > 1000)
+	//~ {
+	  //~ std::cerr << "\nWindow shrunk thousands of times\n";
+	  //~ std::cerr << "\nt_low = " << t_low << ", t_high = " << t_high
+		    //~ << ", working_time = " << working_time << "\n";
+
+	  //~ return t_low;
+	//~ }
+
       working_time = (fwdWorking ? t_low : t_high);
       T tempfL(fL);
       tempfL.stream(working_time);
-    
+
       Iflt deltaT;
       {
 	Iflt f0 = tempfL.F_zeroDeriv(),
 	  f1 = tempfL.F_firstDeriv(),
 	  halff2 = 0.5 * tempfL.F_secondDeriv(),
 	  halff2max = 0.5 * tempfL.F_secondDeriv_max(length);
-	
+
 	if (f0 > 0) halff2max = -halff2max;
-	
+
 	{
 	  Iflt boundEnhancer;
 	  // Enhance bound, no point continuing if the bounds are out of bounds
@@ -63,35 +63,36 @@ Iflt quadRootHunter(const T& fL, Iflt length, Iflt& t_low, Iflt& t_high,
 	    { if (!quadSolve<ROOT_SMALLEST_POSITIVE>(f0, f1, halff2max, boundEnhancer)) break; }
 	  else
 	    if (!quadSolve<ROOT_SMALLEST_NEGATIVE>(f0, f1, halff2max, boundEnhancer)) break;
-	  
+
 	  (fwdWorking ? t_low : t_high) += boundEnhancer;
-	  
+
 	  if (fwdWorking)
 	    { if (!quadSolve<ROOT_SMALLEST_POSITIVE>(f0, f1, halff2, deltaT)) continue; }
 	  else
 	    { if (!quadSolve<ROOT_SMALLEST_NEGATIVE>(f0, f1, halff2, deltaT)) continue; }
 	}
-	
+
       }
-      
-      if (((working_time + deltaT) > t_high) 
+
+      if (((working_time + deltaT) > t_high)
 	  || ((working_time + deltaT) < t_low))
 	continue;
-      
-      for(size_t i(1000); i != 0; --i)
+
+      // Give it 10,000 iterations before we try shrinking the windows again
+      for(size_t i(10000); i != 0; --i)
 	{
 	  working_time += deltaT;
-	  
+
 	  if((working_time > t_high) || (working_time < t_low))
 	    break;
-	  
+
 	  tempfL.stream(deltaT);
-	  
-	  if (!quadSolve<ROOT_SMALLEST_EITHER>(tempfL.F_zeroDeriv(), 
-					       tempfL.F_firstDeriv(), 
+
+	  if (!quadSolve<ROOT_SMALLEST_EITHER>(tempfL.F_zeroDeriv(),
+					       tempfL.F_firstDeriv(),
 					       Iflt(0.5 * tempfL.F_secondDeriv()), deltaT))
 	    break;
-	  
+
 	  if(fabs(deltaT) <  timescale)
 	    return working_time + deltaT;
 	}
@@ -114,17 +115,17 @@ Iflt quadRootHunter(const T& fL, Iflt length, Iflt& t_low, Iflt& t_high,
   **    - If root is invalid, set new concrete t_low just above this found root and go from the top
   */
 template<class T>
-Iflt frenkelRootSearch(const T& fL, Iflt length, Iflt t_low, Iflt t_high, 
+Iflt frenkelRootSearch(const T& fL, Iflt length, Iflt t_low, Iflt t_high,
 		       Iflt tol = 1e-10)
 {
   Iflt root = 0.0;
-	
+
   while(t_high > t_low)
     {
       root = quadRootHunter<T>(fL, length, t_low, t_high, tol);
 
       if (root == HUGE_VAL) return HUGE_VAL;
-      
+
       Iflt temp_high = t_high;
 
       do {
@@ -133,32 +134,32 @@ Iflt frenkelRootSearch(const T& fL, Iflt length, Iflt t_low, Iflt t_high,
 	tempfL.stream(root);
 
 	Iflt Fdoubleprimemax = tempfL.F_secondDeriv_max(length);
-	
+
 	temp_high = root - (fabs(2.0 * tempfL.F_firstDeriv())
 			    / Fdoubleprimemax);
-	
+
 	if ((temp_high < t_low) || (Fdoubleprimemax == 0)) break;
-	
+
 	Iflt temp_root = quadRootHunter<T>(fL, length, t_low, temp_high, tol);
-	
-	if (temp_root == HUGE_VAL) 
+
+	if (temp_root == HUGE_VAL)
 	  break;
-	else 
+	else
 	    root = temp_root;
-	
+
       } while(temp_high > t_low);
-      
+
       // At this point $root contains earliest valid root guess.
       // Check root validity.
       T tempfL(fL);
       tempfL.stream(root);
-      
+
       if (tempfL.test_root(length))
         return root;
       else
         t_low = root + ((2.0 * fabs(tempfL.F_firstDeriv()))
 			/ tempfL.F_secondDeriv_max(length));
     }
-    
+
   return HUGE_VAL;
 }
