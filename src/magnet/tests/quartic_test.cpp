f0dbe0c0252e3dcc520536b7d6df0d8c68f57eaa
Marcus Bannerman <laptop@marcusbannerman.co.uk>
2010-11-10 21:29:01 +0100

    Found some errors to investigate

--- src/magnet/tests/quartic_test.cpp
+++ src/magnet/tests/quartic_test.cpp
@@ -2,6 +2,7 @@
 #include <magnet/math/quartic.hpp>
 #include <vector>
 #include "quartic.c"
+#include <complex>
 
 int main()
 {
@@ -9,6 +10,8 @@ int main()
 
   const size_t nroots = 9;
   double rootvals[nroots] = {-1e6,-1e3, -100, -1, 0, 1, +100, 1e3,1e6};
+
+  std::cout << "\n\n///////////////////////4 real roots//////////////////////////";
 
   size_t counter = 0;
   for (size_t root1 = 0; root1 < nroots; ++root1)
@@ -142,7 +145,133 @@ int main()
 	      std::cout << rts[i] << ",";
 	    
 	  }	  
-
   std::cout << "\nTested " << counter << " roots";
+
+  std::cout << "\n\n///////////////////////4 imaginary roots//////////////////////////";
+
+  counter = 0;
+  for (size_t root1real = 0; root1real < nroots; ++root1real)
+    for (size_t root1im   = 0; root1im < nroots; ++root1im)
+      for (size_t root2real = 0; root2real < nroots; ++root2real)
+	for (size_t root2im   = 0; root2im  < nroots; ++root2im)
+ 	  {
+	    if ((rootvals[root1im] == 0) || rootvals[root2im] == 0) continue;
+
+	    ++counter;
+	    
+	    std::complex<double>
+	      root1val(rootvals[root1real],  rootvals[root1im]),	    
+	      root2val(rootvals[root1real], -rootvals[root1im]),
+	      root3val(rootvals[root2real],  rootvals[root2im]),
+	      root4val(rootvals[root2real], -rootvals[root2im]);
+
+ 	    double a = (-root1val - root2val 
+			- root3val - root4val).real(),
+ 	      b = (root1val * root2val
+		   + root1val * root3val
+		   + root1val * root4val
+		   + root2val * root3val
+		   + root2val * root4val
+		   + root3val * root4val).real(),
+ 	      c = (- root1val * root2val * root3val
+		   - root1val * root2val * root4val
+		   - root1val * root3val * root4val
+		   - root2val * root3val * root4val).real(),
+ 	      d = (root1val * root2val * root3val
+		   * root4val).real();
+	    
+ 	    std::vector<double> roots(4);
+	    
+ 	    size_t rootcount = magnet::math::quarticSolve(a, b, c, d,
+ 							  roots[0], roots[1], 
+							  roots[2], roots[3]);
+
+ 	    sort(roots.begin(), roots.end());
+
+	    if (rootcount == 0) continue;
+
+	    std::cout << "\n\nActual             roots = " 
+		      << root1val.real() << " + " << root1val.imag() <<  " i,"
+		      << root2val.real() << " + " << root2val.imag() <<  " i,"
+		      << root3val.real() << " + " << root3val.imag() <<  " i,"
+		      << root4val.real() << " + " << root4val.imag() <<  " i,";
+	    
+
+	    std::cout << "\nAlgorithm found " << rootcount << ", roots = ";
+	    for (size_t i = 0; i < rootcount; ++i)
+	      std::cout << roots[i] << ",";
+
+	    double rts[4];
+	    int origrootcount = quartic(a,b,c,d,rts);
+	    std::sort(rts, rts+4);
+	    std::cout << "\n Original found " << origrootcount << ", roots = ";
+	    for (size_t i = 0; i < origrootcount; ++i)
+	      std::cout << rts[i] << ",";
+	    
+	    rootcount = magnet::math::ferrariQuarticSolve(a, b, c, d,
+	    						  roots[0], roots[1],
+	    						  roots[2], roots[3]);
+ 	    sort(roots.begin(), roots.end());
+	    
+	    std::cout << "\nFerrari found " << rootcount << ",   roots = ";
+	    for (size_t i = 0; i < rootcount; ++i)
+	      std::cout << roots[i] << ",";
+
+	    origrootcount = ferrari(a,b,c,d,rts);
+	    std::sort(rts, rts+4);
+
+	    std::cout << "\n Original found " << origrootcount << ", roots = ";
+	    for (size_t i = 0; i < origrootcount; ++i)
+	      std::cout << rts[i] << ",";
+	    
+	    rootcount = magnet::math::yacfraidQuarticSolve(a, b, c, d,
+	    						   roots[0], roots[1],
+	    						   roots[2], roots[3]);
+ 	    sort(roots.begin(), roots.end());
+	    std::cout << "\nYacfraid found " << rootcount << ",  roots = ";
+	    for (size_t i = 0; i < rootcount; ++i)
+	      std::cout << roots[i] << ",";
+
+	    origrootcount = yacfraid(a,b,c,d,rts);
+	    std::sort(rts, rts+4);
+
+	    std::cout << "\n Original found " << origrootcount << ", roots = ";
+	    for (size_t i = 0; i < origrootcount; ++i)
+	      std::cout << rts[i] << ",";
+
+	    
+	    rootcount = magnet::math::descartesQuarticSolve(a, b, c, d,
+	    						    roots[0], roots[1], 
+	    						    roots[2], roots[3]);
+ 	    sort(roots.begin(), roots.end());
+	    std::cout << "\nDescartes found " << rootcount << ", roots = ";
+	    for (size_t i = 0; i < rootcount; ++i)
+	      std::cout << roots[i] << ",";
+
+	    origrootcount = descartes(a,b,c,d,rts);
+	    std::sort(rts, rts+4);
+
+	    std::cout << "\n Original found " << origrootcount << ", roots = ";
+	    for (size_t i = 0; i < origrootcount; ++i)
+	      std::cout << rts[i] << ",";
+	    
+	    rootcount = magnet::math::neumarkQuarticSolve(a, b, c, d,
+	    						  roots[0], roots[1],
+	    						  roots[2], roots[3]);
+ 	    sort(roots.begin(), roots.end());
+	    std::cout << "\nNeumark found " << rootcount << ",   roots = ";
+	    for (size_t i = 0; i < rootcount; ++i)
+	      std::cout << roots[i] << ",";
+
+	    origrootcount = neumark(a,b,c,d,rts);
+	    std::sort(rts, rts+4);
+
+	    std::cout << "\n Original found " << origrootcount << ", roots = ";
+	    for (size_t i = 0; i < origrootcount; ++i)
+	      std::cout << rts[i] << ",";
+	    
+	  }	  
+  std::cout << "\nTested " << counter << " roots";
+
   return 0;
 }
