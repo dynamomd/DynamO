b6e586330901364a590fd55f6b9752812369f5a5
Marcus Bannerman <marcus@marcusbannerman.co.uk>
2011-02-28 17:05:48 +0100

    Cleaning up the jam files, now I have configuration testing

---
+++ src/coil/tests/wiimote.cpp
@@ -0,0 +1,25 @@
+/*  DYNAMO:- Event driven molecular dynamics simulator 
+    http://www.marcusbannerman.co.uk/dynamo
+    Copyright (C) 2011  Marcus N Campbell Bannerman <m.bannerman@gmail.com>
+
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    version 3 as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//Test file for CWiid support
+#include "cwiid.h"
+WIBBLE
+int main(int argc, char *argv[])
+{
+  //Something to mean it has to dynamically link
+  cwiid_open(BDADDR_ANY, CWIID_FLAG_CONTINUOUS | CWIID_FLAG_NONBLOCK);
+}
