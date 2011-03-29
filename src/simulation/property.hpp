9f6a188b04d837c84c1f97e724cf6cd70f9bc4e3
Marcus Bannerman <marcus@marcusbannerman.co.uk>
2011-03-29 10:08:48 +0200

    Properties need a rethink

--- src/simulation/property.hpp
+++ src/simulation/property.hpp
@@ -20,47 +20,66 @@
 #include <algorithm>
 #include <magnet/exception.hpp>
 #include <magnet/xmlwriter.hpp>
+#include <magnet/thread/refPtr.hpp>
 
-class Property
+
+/*! \brief A container to hold a property every particle in the system
+ * has.
+ *
+ * 
+ */
+class Property : public std::vector<double>
 {
 public:
   Property(std::string name, size_t N):
-    _name(name),
-    _storage(N)
+    std::vector<double>(N),
+    _name(name)
   {}
   
-  inline double& getProperty(size_t ID) { return _storage[ID]; }
-  inline const double& getProperty(size_t ID) const { return _storage[ID]; }
+  inline double& getProperty(size_t ID) { return operator[](ID); }
+  inline const double& getProperty(size_t ID) const { return operator[](ID); }
  
   bool operator==(const std::string& str) const { return _name == str; }
 
   void XMLOutput(xml::XmlStream& XML, const size_t ID) const
-  {
-    XML << xml::attr(_name) << _storage[ID];
-  }
+  { XML << xml::attr(_name) << _storage[ID]; }
 
 protected:
   std::string _name;
-  std::vector<double> _storage;
 };
 
-class PropertyStore : public std::vector<Property>
+class PropertyStore : public std::vector<magnet::thread::RefPtr<Property> >
 {
 public:
-  const Property& getProperty(const std::string& name) const
+  const magnet::thread::RefPtr<Property>& getProperty(const std::string& name) const
   {
-    const_iterator it = std::find(begin(), end(), name);
-    
-    if (it == end()) M_throw() << "Could not find the property";
-    
+    const_iterator it = std::find(begin(), end(), name);    
+    if (it == end()) M_throw() << "Could not find the property";    
     return *it;
   }
 
   void XMLOutput(xml::XmlStream& XML, const size_t ID) const
   {
     for (const_iterator iPtr = begin(); iPtr != end(); ++iPtr)
-      iPtr->XMLOutput(XML, ID);
+      (*iPtr)->XMLOutput(XML, ID);
   }
   
 private:
+};
+
+
+class PropertyHandle
+{
+public:  
+  virtual double getProperty(size_t ID) = 0;
+};
+
+class PropertyReference : public PropertyHandle
+{
+public:  
+  virtual double getProperty(size_t ID) 
+  { return _ref->getProperty(ID); }
+
+protected:
+  const magnet::thread::RefPtr<Property> _ref;
 };
