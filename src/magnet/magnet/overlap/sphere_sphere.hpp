c32254df52528b54f7268549b9ad45861dfb6228
Marcus Bannerman <marcus@marcusbannerman.co.uk>
2011-05-21 21:28:39 +0200

    Another overlap test

---
+++ src/magnet/magnet/overlap/sphere_sphere.hpp
@@ -0,0 +1,47 @@
+/*  dynamo:- Event driven molecular dynamics simulator 
+    http://www.marcusbannerman.co.uk/dynamo
+    Copyright (C) 2011  Marcus N Campbell Bannerman <m.bannerman@gmail.com>
+
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    version 3 as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include <magnet/math/vector.hpp>
+
+namespace magnet {
+  namespace overlap {
+    //! A point-sphere overlap test.
+    //!
+    //! This function assumes the point location passed is relative to
+    //! the sphere's center.
+    //!
+    //! \param S The point's relative position.
+    //! \param d The diameter of the sphere.
+    //! \return Whether the point is inside the sphere.
+    inline bool point_sphere(const Vector& P, 
+			     const double d)
+    { return P.nrm2() <= d * d; }
+
+    //! A sphere-sphere overlap test.
+    //!
+    //! This function assumes the sphere location passed is relative to
+    //! the other sphere's center.
+    //!
+    //! \param S The other sphere's relative position.
+    //! \param d The average diameter of the spheres.
+    //! \return Whether the spheres are overlapping.
+    inline bool sphere_sphere(const Vector& P, 
+			     const double d)
+    { return point_sphere(P, d); }
+  }
+}
