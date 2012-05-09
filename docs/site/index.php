96e3c1c850987b318f381332f56fe27bed4edd6b
Marcus Bannerman <m.bannerman@gmail.com>
2012-05-09 18:24:29 +0100

    Cleaned up the borders

--- docs/site/index.php
+++ docs/site/index.php
@@ -22,48 +22,48 @@
   </head>
   <body>
     <div id="wrapper" style="position:relative;">
-      <div class="header">
-	<div style="position:absolute; top:0px; left:15px; width:15px; height:115px;">
-	  <div class="sprite" style="width:15px; height:15px; position:absolute; top:0;"></div>
-	  <div style="height:85px; margin-top:15px; margin-bottom:15px; background: #ffffff; "></div>
-	  <div class="sprite" style="width:15px; height:15px; position:absolute; bottom:0px; background-position: 0px -15px;"></div>
-	</div>
 
-	<a href="">
-	  <div id="headermiddle" style="margin-left:30px; height:115px; background: no-repeat #ffffff url(images/sitelogo.png); margin-right:30px;">
-	  </div>
-	</a>
-	<div style="position:absolute; top:0px; right: 15px; width:15px; height:115px;">
-	  <div class="sprite" style="width:15px; height:15px; background-position: -15px 0px;"></div>
-	  <div style="width:15px; height:85px; background:#ffffff; "></div>
-	  <div class="sprite" style="width:15px; height:15px; background-position: -15px -15px;"></div>
+      <!-- HEADER AND LOGO -->
+      <div id="header">
+	<div class="borderleft">
+	  <div class="topleftcornerborder sprite"></div>
+	  <div class="verticalborder"></div>
+	  <div class="bottomleftcornerborder sprite"></div>
+	</div>
+	<a href="" class="bordercentre" style="background-repeat:no-repeat; background-image:url(images/sitelogo.png);"></a>
+	<div class="borderright">
+	  <div class="toprightcornerborder sprite"></div>
+	  <div class="verticalborder"></div>
+	  <div class="bottomrightcornerborder sprite"></div>
 	</div>
       </div>
-      <div style="width:100%; height: 30px;"></div>
+
+
+      <!-- MENU AND CONTENT -->
+      <div style="clear:both; width:100%; height: 30px;"></div>
       <div id="leftmenu" style="background:#ffffff; position:absolute; left:0; margin-left:30px; width:150px;">Left Menu</div>
       <div id="content" style=" padding-left:200px;">
 	<div style="background:#ffffff; height:200px;"></div>
       </div>
       <div id="wrapperfooterpad"></div>
     </div>
-    <div id="footer">
-      
-      <div style="position:absolute; top:0px; left:15px; width:15px; height:100%;">
-	<div class="sprite" style="width:15px; height:15px; position:absolute; top:0;"></div>
-	<div style="height:5px; margin-top:15px; margin-bottom:15px; background: #ffffff; "></div>
-	<div class="sprite" style="width:15px; height:15px; position:absolute; bottom:0px; background-position: 0px -15px;"></div>
-      </div>
 
-      <div style="margin-left:30px; margin-right:30px; background:#ffffff; height:35px;">
+    <!-- FOOTER -->
+    <div id="footer">
+      <div class="borderleft">
+	<div class="topleftcornerborder sprite"></div>
+	<div class="verticalborder"></div>
+	<div class="bottomleftcornerborder sprite"></div>
+      </div>
+      <div class="bordercentre">
 	Copyright &copy; 2012
 	<a href="http://validator.w3.org/check?uri=referer" class="footerlogo" style="background: url('images/valid-xhtml11-blue.png'); float:right;"></a>
 	<a href="http://jigsaw.w3.org/css-validator/check/referer" class="footerlogo" style="background: url('images/vcss-blue.png'); float:right;"></a>
       </div>
-
-      <div style="position:absolute; top:0px; right: 15px; width:15px;">
-	<div class="sprite" style="width:15px; height:15px; background-position: -15px 0px;"></div>
-	<div style="width:15px; height:5px; background:#ffffff; "></div>
-	<div class="sprite" style="width:15px; height:15px; background-position: -15px -15px;"></div>
+      <div class="borderright">
+	<div class="toprightcornerborder sprite"></div>
+	<div class="verticalborder"></div>
+	<div class="bottomrightcornerborder sprite"></div>
       </div>
     </div>
   </body>
--- docs/site/style/style.php
+++ docs/site/style/style.php
@@ -11,8 +11,7 @@ body { margin: 0; padding: 0; height: 100%; background: #000000; }
 #wrapper { min-height:100%; overflow-y:visible; }
 #wrapperfooterpad { height:<?php echo $footerheight; ?>; width:100%; clear:both; }
 
-#leftmenu {  }
-#content { }
+#header { position:relative; height:115px; }
 
 #footer {
     height:<?php echo $footerheight; ?>; 
@@ -21,4 +20,15 @@ body { margin: 0; padding: 0; height: 100%; background: #000000; }
 }
 
 .footerlogo { border:0; width:88px; height:31px }
+
 .sprite { background-image: url(../images/csssprites.png); }
+
+/*     Styling of the round edged boxes       */
+.borderleft { left:15px; position:absolute; top:0px; width:15px; height:100%; }
+.borderright { right:15px; position:absolute; top:0px; width:15px; height:100%; }
+.bordercentre { height:100%; background-color:#ffffff; position:absolute; left:30px; right:30px;}
+.topleftcornerborder { width:15px; height:15px; position:absolute; top:0; background-position: 0px 0px; }
+.bottomleftcornerborder { width:15px; height:15px; position:absolute; bottom:0px; background-position: 0px -15px; }
+.toprightcornerborder { width:15px; height:15px; position:absolute; top:0; background-position: -15px 0px; }
+.bottomrightcornerborder { width:15px; height:15px; position:absolute; bottom:0px; background-position: -15px -15px; }
+.verticalborder { background: #ffffff; position:absolute; top:15px; bottom:15px;left:0px; right:0px; }
