2f0b1c0ccbed568dd275febebb3013002b3e3e1e
Marcus Bannerman <m.bannerman@gmail.com>
2012-05-11 12:41:24 +0100

    more html errors fixed

--- docs/site/index.php
+++ docs/site/index.php
@@ -14,7 +14,6 @@ if (!file_exists("pages/".$page.".php"))
 $in_template=1;
 include_once("pages/".$page.".php");
 ?>
-
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
