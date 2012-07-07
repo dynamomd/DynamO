9d0eaf18906a7eef86142963b9102b964fca82db
Marcus Bannerman <m.bannerman@gmail.com>
2012-07-07 15:55:05 +0100

    The correllator classes are now functionally complete.

--- src/magnet/magnet/math/correlators.hpp
+++ src/magnet/magnet/math/correlators.hpp
@@ -17,9 +17,11 @@
 
 #pragma once
 #include <boost/circular_buffer.hpp>
+#include <boost/foreach.hpp>
 #include <vector>
 #include <utility>
 #include <exception>
+#include <tr1/tuple>
 
 namespace magnet {
   namespace math {
@@ -108,6 +110,14 @@ namespace magnet {
 	return avg_correlator;
       }
 
+      /*! \brief Returns the number of samples collected for the
+          \f$j\f$th correlation of \f$f_{1,2}(j)\f$. 
+      */
+      size_t getSampleCount(size_t i) const
+      {
+	return _count - i;
+      }
+
     protected:
       boost::circular_buffer<std::pair<T, T> > _sample_history;
       std::vector<T> _correlator;
@@ -145,13 +155,13 @@ namespace magnet {
       typedef Correlator<T> Base;
     public:
       TimeCorrelator(double sample_time, size_t length): 
-	Base(length)
+	Base(length),
+	_sample_time(sample_time)
       { 
 	if (sample_time <= 0)
 	  throw std::runtime_error("TimeCorrelator requires a positive, non-zero sample time");
+
 	clear(); 
-	//Has to come after the clear
-	_sample_time = sample_time;
       }
 
       void addImpulse(const T& val1, const T& val2)
@@ -188,17 +198,137 @@ namespace magnet {
 	Base::clear();
 	_freestream_values = std::pair<T,T>();
 	_W_sums = std::pair<T,T>();
-	_sample_time = _current_time = 0;
+	_current_time = 0;
 	
       }
 
       using Base::getAveragedCorrelator;
+      using Base::getSampleCount;
+
+      double getSampleTime() const { return _sample_time; }
 
     protected:
       std::pair<T,T> _freestream_values;
       std::pair<T,T> _W_sums;
       double _sample_time;
       double _current_time;
+    };
+
+    template<class T>
+    class LogarithmicTimeCorrelator
+    {
+      typedef std::vector<TimeCorrelator<T> > Container;
+    public:
+      LogarithmicTimeCorrelator(double sample_time, size_t length, size_t scaling = 2):
+	_sample_time(sample_time),
+	_length(length),
+	_scaling(scaling)
+      {
+	clear();
+      }
+    
+      void clear()
+      {
+	_current_time = 0;
+	_freestream_values = _freestream_sum = _impulse_sum= std::pair<T,T>();
+	_sample_time /= (1 << _correlators.size());
+	_correlators.clear();
+      }
+
+      void addImpulse(const T& val1, const T& val2)
+      {
+	_impulse_sum.first += val1; 
+	_impulse_sum.second += val2;
+
+	BOOST_FOREACH(TimeCorrelator<T>& correlator, _correlators)
+	  correlator.addImpulse(val1, val2);
+      }
+
+      void setFreeStreamValue(const T& val1, const T& val2)
+      {
+	_freestream_values = std::pair<T,T>(val1, val2);
+
+	BOOST_FOREACH(TimeCorrelator<T>& correlator, _correlators)
+	  correlator.setFreeStreamValue(val1, val2);
+      }
+
+      void freeStream(const double dt)
+      {
+	//Check if we need to add a new correlator
+	while ((_current_time + dt) >= _sample_time)
+	  {
+	    //Add a new correlator
+	    _correlators.push_back(TimeCorrelator<T>(_sample_time, _length));
+	    _sample_time *= _scaling;
+	    
+	    //Pretend the correlator has actually been here all along, gathering impulse data
+	    TimeCorrelator<T>& new_correlator = _correlators.back();
+	    new_correlator.addImpulse(_impulse_sum.first, _impulse_sum.second);
+
+	    //Also fake a freestream integration, it is fine to fake
+	    //this as the correlators cant resolve smaller than a
+	    //_sample_time.
+	    new_correlator.setFreeStreamValue(_freestream_sum.first / _current_time, _freestream_sum.second / _current_time);
+	    new_correlator.freeStream(_current_time);
+
+	    //Set the correct free streaming value
+	    new_correlator.setFreeStreamValue(_freestream_values.first, _freestream_values.second);
+	  }
+
+	BOOST_FOREACH(TimeCorrelator<T>& correlator, _correlators)
+	  correlator.freeStream(dt);
+
+	_freestream_sum.first += _freestream_values.first * dt;
+	_freestream_sum.second += _freestream_values.second * dt;
+	_current_time += dt;
+      }
+
+      struct Data
+      {
+	Data(double t, size_t sc, T v): time(t), sample_count(sc), value(v) {}
+
+	double time;
+	size_t sample_count;
+	T value;
+      };
+
+      std::vector<Data> getAveragedCorrelator()
+      {
+	std::vector<Data> avg_correlator;
+
+	if (!_correlators.empty())
+	  {
+	    {
+	      std::vector<T> result = _correlators.front().getAveragedCorrelator();
+	      
+	      for (size_t i(0); i < result.size(); ++i)
+		avg_correlator.push_back(Data(_correlators.front().getSampleTime() * (i+1), 
+					      _correlators.front().getSampleCount(i), result[i]));
+	    }
+
+	    //Now copy the rest of the correlators
+	    for (size_t i(1); i < _correlators.size(); ++i)
+	      {
+		std::vector<T> result = _correlators[i].getAveragedCorrelator();
+		for (size_t j(_length / _scaling); j < result.size(); ++j) 
+		  avg_correlator.push_back(Data(_correlators[i].getSampleTime() * (j+1),
+						_correlators[i].getSampleCount(j), result[j]));
+	      }
+	  }
+	return avg_correlator;
+      }
+
+
+    protected:
+      double _sample_time;
+      double _current_time;
+      size_t _length;
+      size_t _scaling;
+      std::pair<T,T> _freestream_values;
+      std::pair<T,T> _impulse_sum;
+      std::pair<T,T> _freestream_sum;
+      
+      std::vector<TimeCorrelator<T> > _correlators;
     };
   }
 }
