69d2ee5597cd222cb56024a84d6ba7726ba307b9
Marcus Bannerman <m.bannerman@gmail.com>
2012-07-13 10:04:57 +0100

    removed more unused scripts and a defunct replica exchange optimisation program

--- scripts/optimise_replex_temps.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-../../replex_opt | gawk '{print "bzcat replex."$1".xml.bz2 | xml ed -u '"'/DYNAMOconfig/Dynamics/SystemEvents/System/@Temperature'"' -v \""$2"\" | bzip2 > replex."$1".xml.bz2"}' | /bin/bash
--- scripts/xargsrender.sh
+++
@@ -1,4 +0,0 @@
-#Run this script like so ls *.pov | xargs -L1 -P4 xargsrender.sh
-if [ ! -e ${@%.pov}.png ]; then 
-    povray +H768 +W1024 -D $@
-fi
