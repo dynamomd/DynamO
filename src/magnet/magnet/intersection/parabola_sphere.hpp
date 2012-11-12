ece043966476fd88261977fbde5c455e7323bfc2
Marcus Bannerman <m.bannerman@gmail.com>
2012-11-12 20:34:24 +0000

    Cleaned up the bisection routine and reimplemented the positive overlap function according to the paper

--- src/magnet/magnet/intersection/parabola_sphere.hpp
+++ src/magnet/magnet/intersection/parabola_sphere.hpp
@@ -26,7 +26,7 @@ namespace magnet {
       struct QuarticFunc
       {
       public:
-	inline double operator()(double t)
+	inline double operator()(double t) const
 	{
 	  return (((coeffs[0] * t + coeffs[1]) * t + coeffs[2]) * t + coeffs[3]) * t + coeffs[4];
 	}
@@ -35,13 +35,14 @@ namespace magnet {
       };
     }
 
-    //! \brief A parabolic(ray)-sphere intersection test with backface culling.
-    //!
-    //! \param T The origin of the ray relative to the sphere center.
-    //! \param D The direction/velocity of the ray.
-    //! \param G The acceleration of the ray.
-    //! \param r The radius of the sphere.
-    //! \return The time until the intersection, or HUGE_VAL if no intersection.
+    /*! \brief A parabolic(ray)-sphere intersection test with backface culling.
+      
+      \param T The origin of the ray relative to the sphere center.
+      \param D The direction/velocity of the ray.
+      \param G The acceleration of the ray.
+      \param r The radius of the sphere.
+      \return The time until the intersection, or HUGE_VAL if no intersection.
+    */
     inline double parabola_sphere_bfc(const math::Vector& T,
 				      const math::Vector& D,
 				      const math::Vector& G,
@@ -52,96 +53,41 @@ namespace magnet {
       
       double DdotT = (D | T);
 
-      magnet::math::Bisect<detail::QuarticFunc> quartic;
-      quartic.coeffs[0] = 0.25 * G.nrm2();
-      quartic.coeffs[1] = G | D;
-      quartic.coeffs[2] = D.nrm2() + (G | T);
-      quartic.coeffs[3] = 2 * DdotT;
-      quartic.coeffs[4] = T.nrm2()- r * r;
+      detail::QuarticFunc f;
+      f.coeffs[0] = 0.25 * G.nrm2();
+      f.coeffs[1] = G | D;
+      f.coeffs[2] = D.nrm2() + (G | T);
+      f.coeffs[3] = 2 * DdotT;
+      f.coeffs[4] = T.nrm2()- r * r;
       
       //We calculate the roots of the cubic differential of F
       //\f$F=A t^4 + B t^3 + C t^2 + D t + E == 0\f$ taking the differential gives
       //\f$F=4 A t^3 + 3 B t^2 + 2C t + D == 0\f$ and normalizing the cubic term gives
       //\f$F=t^3 + \frac{3 B}{4 A} t^2 + \frac{2C}{4 A} t + \frac{D}{4 A} == 0\f$
       double roots[3];
-      size_t rootCount = magnet::math::cubicSolve(quartic.coeffs[1] * 3 / (4 * quartic.coeffs[0]),
-						  quartic.coeffs[2] * 2 / (4 * quartic.coeffs[0]),
-						  quartic.coeffs[3] * 1 / (4 * quartic.coeffs[0]),
+      size_t rootCount = magnet::math::cubicSolve(f.coeffs[1] * 3 / (4 * f.coeffs[0]),
+						  f.coeffs[2] * 2 / (4 * f.coeffs[0]),
+						  f.coeffs[3] * 1 / (4 * f.coeffs[0]),
 						  roots[0], roots[1], roots[2]);
       
       //Sort the roots in ascending order
       std::sort(roots, roots + rootCount);
-      //The roots are (in order) a minimum, (and if we have 3 roots), a
-      //local maximum, then another minimum
-      
-      //We need to define the dynamics of overlapping particles
-      //
-      //We follow the convention that, if particles are overlapped and
-      //approaching, we collide them now.
-      //
-      //But, there are some cases where the particle is overlapped,
-      //receeding but will not escape the overlap before turning around
-      //and approaching again.
-      //
-      //Here we make another choice, we collide whenever the particle is
-      //touching/overlapping and approaching. We may get some kind of
-      //inelastic collapse, which is fair, but it may be avoided by
-      //sleeping the particles or by other dynamics
-      if (quartic(0) <= 0)
-	{//We're overlapping! We need the overlapped dynamics!
-	  if (DdotT < 0)
-	    //We're overlapping and approaching! Cause an instantaneous collision
+      if ((roots[0] > 0) && (f(roots[0]) < 0))
+	{
+	  if (f(0) <= 0) 
 	    return 0;
-      
-	  //We're overlapping but receeding. If there's only one cubic
-	  //root, then we're fine, we've passed the local minimum (we're
-	  //receeding) so we'll escape the particle (never to return) soon
-	  //enough, return no collision
-	  if (rootCount == 1) return HUGE_VAL;
-
-	  //We have a local minimum, maximum and minimum. We must be after
-	  //the first local minimum as we're overlapping but receeding. If
-	  //the maximum is in the past, and we're receeding, we must be
-	  //past the second minimum and we will only get further away! so
-	  //return no collision
-	  if (roots[1] < 0) return HUGE_VAL;
-
-	  //The local maximum is in the future (or now). Check if we will be
-	  //outside the particle then.
-	  if (quartic(roots[1]) > 0)
-	    { //We will be outside the particle, so check if we re-enter it!
-	      if (quartic(roots[2]) < 0)
-		{//We do reenter! So return the time of this reentry as the next event
-		  return std::max(0.0, quartic.bisectRoot(roots[1], roots[2], rootthreshold));
-		}
-	      //We wont reenter, so return no intersection
-	      return HUGE_VAL;
-	    }
-      
-	  //Our local maxima is in the future, and it's still an
-	  //overlapped state!  Return an event at the local maximum, it's
-	  //the furthest away we're going to get without approaching
-	  //again. Either we'll have another event before we reach it or
-	  //we'll have to handle it by sleeping the particle.
-	  return roots[1]; //return the local maximum 
+	  else
+	    return magnet::math::bisect(f, 0, roots[0], rootthreshold);
 	}
-      
-      //We're not overlapping, so check for standard events  
 
-      //Check the first minimum (we always have one), if it's in the
-      //future check that we overlap then, and then bisect the event
-      if ((roots[0] >= 0) && (quartic(roots[0]) <= 0))
-	return std::max(0.0, quartic.bisectRoot(0, roots[0], rootthreshold));
-
-      //Check the second minimum if we have one. When bisecting we use
-      //either now or the local maximum, depending on which is later as
-      //the first point.
-      if ((rootCount > 1)
-	  && (roots[2] > 0)
-	  && (quartic(roots[2]) < 0)
-	  && (quartic(std::max(0.0, roots[1])) >= 0))
-	return std::max(0.0, quartic.bisectRoot(std::max(0.0, roots[1]), roots[2], rootthreshold));
-  
+      if ((rootCount == 3) && (roots[2] > 0) && (f(roots[2]) < 0))
+	{
+	  double tmin = std::max(0.0, roots[1]);
+	  if (f(tmin) <= 0)
+	    return tmin;
+	  else
+	    return magnet::math::bisect(f, tmin, roots[2], rootthreshold);
+	}
       return HUGE_VAL;
     }
   }
