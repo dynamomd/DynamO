26839caecdc61e7332705e9746a5c5a541644939
Marcus Bannerman <m.bannerman@gmail.com>
2014-02-28 17:54:06 +0000

    More work

---
+++ src/magnet/magnet/intersection/polynomial.hpp
@@ -0,0 +1,58 @@
+/*  dynamo:- Event driven molecular dynamics simulator 
+    http://www.dynamomd.org
+    Copyright (C) 2011  Marcus N Campbell Bannerman <m.bannerman@gmail.com>
+
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    version 3 as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include <algorithm>
+
+namespace magnet {
+  namespace intersection {
+    namespace detail {
+      inline double firstOrder(const double f0, const double f1) {
+	if (f1 >= 0) return HUGE_VAL;
+	return std::max(0, - f0 / f1);
+      }
+
+      inline double secondOrder(const double f0, const double f1, const double f2) {
+	if (f2 == 0) return firstOrder(f0, f1);
+	
+	double arg = f1 * f1 - f2 * f0;
+	if (f2 < 0) {
+	  if (arg <= 0) return std::max(0, -f1 / f2);
+	  if (f1 > 0)
+	    return std::max(0, (-f1 - std::sqrt(arg)) / f2);
+	  else
+	    return std::max(0, f0 / (-f1 + std::sqrt(arg)));
+	} else {//f2 > 0
+	  if ((f1 >= 0) || arg <= 0) return HUGE_VAL;
+	  return std::max(0, - f0 / (-f1 + std::sqrt(arg)));
+	}
+      }
+
+      /* class F { template<size_t deriv> double eval(double deltat); template<size_t deriv, bool decreasing> nextRoot(double deltat); } */
+
+      template<class F>
+      inline double stableAlgorithm(const F& f)
+      {
+	if (f.eval<0>(0) > 0) return f.nextRoot<0, true>(0);
+        if (f.eval<1>(0) < 0) return 0;
+	const double tderiv = f.nextRoot<1, true>(0);
+	if (tderiv == HUGE_VAL) return HUGE_VAL;
+	if (f.eval<0>(tderiv) > 0) return f.nextRoot<0, true>(tderiv);
+      }
+    }
+  }
+}
--- src/magnet/tests/triangle_intersection.cpp
+++ src/magnet/tests/triangle_intersection.cpp
@@ -23,7 +23,7 @@ Vector random_unit_vec() {
 const size_t testcount = 1000;
 const double errlvl = 1e-8;
 
-std::pair<double, double> ray_wall(const Vector& relPos, const Vector& relVel, 
+std::pair<double, double> ray_wall_bounds(const Vector& relPos, const Vector& relVel, 
 				   const Vector& normal, const double dist)
 {
   using namespace std;
@@ -35,21 +35,44 @@ std::pair<double, double> ray_wall(const Vector& relPos, const Vector& relVel,
   return pair<double, double>(min(val1, val2), max(val1, val2));
 }
 
-
-BOOST_AUTO_TEST_CASE( TimeToEvent_Test )
+double ray_prism(const Vector[3]& V, const Vector& pos, 
+		 const Vector& vel, const double dist)
 {
-  const Vector V[] = {random_vec(), random_vec(), random_vec()};
+  using namespace std;
   const Vector M = (V[0] + V[1] + V[2]) / 3;
   Vector Vrel[3];
   double Vdist[3];
+
   for (size_t i(0); i<3; ++i) {
     Vrel[i] = V[i] - M;
     Vdist[i] = Vrel[i].nrm();
     Vrel[i] /= Vdist[i];
   }
 
+  double tmin = HUGE_VAL;
+  double tmax = - HUGE_VAL;
+  Vector relPos = pos - M;
+
+  //Test against the bounds given by the three walls
+  for (size_t i(0); i<3; ++i) {
+    auto bounds = ray_wall(relPos, vel, Vrel[i], Vdist[i]);
+    tmin = min(bounds.first, tmin);
+    tmax = max(bounds.second, tmax);
+  }
+  
+  //Test against the plane the triangle is in
   Vector N = Vrel[0] ^ Vrel[1];
   N /= N.nrm();
+  auto bounds = ray_wall(relPos, vel, N, dist);
+  tmin = min(bounds.first, tmin);
+  tmax = max(bounds.second, tmax);
+  
+}
+
+
+BOOST_AUTO_TEST_CASE(TimeToEvent_Test)
+{
+  const Vector V[] = {random_vec(), random_vec(), random_vec()};
 
   RNG.seed();
   auto out = ray_wall(Vector(1.5141, 0, 0), Vector(-0.92378417, 0, 0), Vector(-1, 0, 0), 0.5);
