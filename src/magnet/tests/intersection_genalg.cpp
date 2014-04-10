dd8b8c15f14fcc2999ebc4306d70d09700b53c85
Marcus Bannerman <m.bannerman@gmail.com>
2014-04-10 21:25:51 +0100

    Separated the tests to allow better performance tracking

--- src/magnet/tests/intersection_genalg.cpp
+++ src/magnet/tests/intersection_genalg.cpp
@@ -24,10 +24,6 @@ Vector random_unit_vec() {
   return vec / vec.nrm();
 }
 
-const size_t testcount = 1000;
-const double errlvl = 1e-8;
-
-
 using namespace magnet::intersection;
 
 template<size_t Order>
@@ -85,53 +81,5 @@ BOOST_AUTO_TEST_CASE( GravitySphere_Test )
       { BOOST_CHECK(false); }
     else if (!std::isinf(radical_root))
       { BOOST_CHECK_CLOSE(radical_root, numerical_root.second, 1e-12); }
-  }
-}
-
-BOOST_AUTO_TEST_CASE( OffCentreSphere_Test )
-{
-  RNG.seed(5489u);
-  using namespace magnet::math;
-
-  const Vector rij1(0.33930816635469108, 1.971007348602491, 0);
-  const Vector vij(1.1608942531073687, -4.0757606085691398, 0);
-  const Vector angvi(-0,-0,-1.0326096458374654);
-  const Vector angvj(0,0,3.0759235803301794);
-  const Vector relativeposi1(0.19838653763498912, -0.45895836596057499, 2.2204460492503128e-16);
-  const Vector relativeposj1(0.32578919839301484, 0.37929065136177137, 0);
-  const double diameteri=1, diameterj=1, maxdist = 2;
-
-  magnet::intersection::detail::OffcentreSpheresOverlapFunction f1(rij1, vij, angvi, angvj, relativeposi1, relativeposj1, diameteri, diameterj, maxdist);
-  auto result1 = magnet::intersection::nextEvent(f1, 0, 0.49421681707429921);
-  //Check against verified result
-  BOOST_CHECK(result1.first);
-  BOOST_CHECK_CLOSE(result1.second, 0.032812502395565935, 1e-10);
-
-  //Check that time shifts do not yield different roots
-  const size_t tests = 1000000;
-  for (size_t i(0); i < tests; ++i){
-    const double dt = result1.second * dist01(RNG);
-    const Vector rij2 = rij1 + dt * vij;
-    const Vector relativeposi2 = Rodrigues(angvi * dt) * relativeposi1;
-    const Vector relativeposj2 = Rodrigues(angvj * dt) * relativeposj1;
-    magnet::intersection::detail::OffcentreSpheresOverlapFunction f2(rij2, vij, angvi, angvj, relativeposi2, relativeposj2, diameteri, diameterj, maxdist);
-    auto result2 = magnet::intersection::nextEvent(f2, 0, 0.81815864721356835);
-    
-    BOOST_CHECK(result2.first);
-    BOOST_CHECK_CLOSE(result2.second + dt, result1.second, 1e-10);
-  }
-
-  //Check that exceeding the predicted event time by a small amount
-  //results in instant collisions.
-  for (size_t i(0); i < tests; ++i){
-    const double dt = result1.second * (1 + 0.01 * dist01(RNG));
-    const Vector rij2 = rij1 + dt * vij;
-    const Vector relativeposi2 = Rodrigues(angvi * dt) * relativeposi1;
-    const Vector relativeposj2 = Rodrigues(angvj * dt) * relativeposj1;
-    magnet::intersection::detail::OffcentreSpheresOverlapFunction f2(rij2, vij, angvi, angvj, relativeposi2, relativeposj2, diameteri, diameterj, maxdist);
-    auto result2 = magnet::intersection::nextEvent(f2, 0, 0.81815864721356835);
-    
-    BOOST_CHECK(result2.first);
-    BOOST_CHECK(result2.second == 0);
   }
 }
