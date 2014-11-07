3c74a788ca070d9bdcf6886bf8ed23cf8b61304b
Marcus Bannerman <m.bannerman@gmail.com>
2014-11-07 20:26:28 +0000

    Trying to get my own cdash server to work

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,10 +1,7 @@
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
-## # The following are required to uses Dart and the Cdash dashboard
-##   ENABLE_TESTING()
-##   INCLUDE(CTest)
-set(CTEST_PROJECT_NAME "DynamOMD")
+set(CTEST_PROJECT_NAME "DynamO")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
--- nightly.cmake
+++ nightly.cmake
@@ -1,20 +1,20 @@
 ##This is a script file to automate compilation and execution of
 ##DynamO tests. This is used for continuous integration testing.
 
-set(BRANCH "experimental")
-#Please set the following to a description of the test system (i.e. CentOS6.5)
-set(CTEST_SITE "UserSite")
+set(CTEST_SITE "UserComputer")
 set(CTEST_BUILD_NAME "linux-gcc-default")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_CONFIGURATION "Release")
 
+set(BRANCH "experimental")
+
+#Please set the following to a description of the test system (i.e. CentOS6.5)
 ##########################################################################
 # Site-specific setup
 ##########################################################################
 set(CTEST_SOURCE_DIRECTORY "/home/mjki2mb2/CTest/dynamo/${BRANCH}/source")
 set(CTEST_BINARY_DIRECTORY "/home/mjki2mb2/CTest/dynamo/${BRANCH}/build")
 
-set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set(CTEST_BUILD_CONFIGURATION "Release")
-set(CTEST_BUILD_FLAGS "-j1")
 
 ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 SET (CTEST_COMMAND "ctest")
@@ -33,7 +33,6 @@ endif()
 set(CTEST_UPDATE_COMMAND "${CTEST_GIT_COMMAND}")
 
 set(CTEST_CONFIGURE_COMMAND "${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}")
-set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} -DWITH_TESTING:BOOL=ON \"-DCTEST_BUILD_FLAGS:STRING=${CTEST_BUILD_FLAGS}\"")
 set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"-G${CTEST_CMAKE_GENERATOR}\"")
 set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"${CTEST_SOURCE_DIRECTORY}\"")
 
