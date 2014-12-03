24e395c0f5ed7f06595430f9cba26efd509ec382
Marcus Bannerman <m.bannerman@gmail.com>
2014-12-03 19:21:33 +0000

    Swapped the in progress file and the original

--- src/magnet/magnet/intersection/polynomial.hpp
+++ src/magnet/magnet/intersection/polynomial.hpp
@@ -16,110 +16,106 @@
 */
 
 #pragma once
-#include <magnet/math/polynomial.hpp>
+#include <algorithm>
+#include <array>
+#include <magnet/math/cubic.hpp>
+#include <magnet/math/bisect.hpp>
 
 namespace magnet {
   namespace intersection {
-    inline double nextEvent(const Polynomial<1>& f) {
-      //If the gradient is not negative now, it never will be
-      if (f[1] >= 0) return HUGE_VAL;
-      //Return the time of the root, or now if we're past the root
-      return std::max(0.0, - f[0] / f[1]);
-    }
-    
-    inline double nextEvent(Polynomial<2> f) {
-      //If the polynomial is linear, drop to that solution
-      if (f[2] == 0) return nextEvent(Polynomial<1>(f));
-      
-      const double arg = f[1] * f[1] - 2 * f[2] * f[0];
-
-      if (f[2] < 0) {
-	//Polynomial limits towards overlap at t -> +inf
-	
-	//If there are no roots, it never escapes overlap, return the
-	//time of the turning point or now if it is in the past
-	if (arg <= 0) return std::max(0.0, -f[1] / f[2]);
-
-	//There are roots. select a stable form of the quadratic to
-	//compute the largest root.
-	if (f[1] > 0)
-	  return std::max(0.0, (-f[1] - std::sqrt(arg)) / f[2]);
-	else
-	  return std::max(0.0, 2 * f[0] / (-f[1] + std::sqrt(arg)));
-      } 
-      /* else (f[2] > 0)*/
-
-      //Polynomial limits away from overlap at t -> +inf
-      const double arg = f[1] * f[1] - 2 * f[2] * f[0];
-
-      //Interactions only happen if there are roots and we're in the
-      //region between the first root and the turning point
-      if ((f[1] >= 0) || arg <= 0) return HUGE_VAL;
-
-      //Return the time of the root using a stable quadratic formula
-      return std::max(0.0, 2 * f[0] / (-f[1] + std::sqrt(arg)));
-    }
-
-      inline double nextEvent(const Polynomial<3>& f)
+    namespace detail {
+      template<size_t Order>
+      struct PolynomialFunction: public std::array<double, Order + 1>
       {
-      //If the polynomial is quadratic, drop to that solution
+	template <typename... T>
+	PolynomialFunction(T... ts) : std::array<double, Order + 1>({{ts...}}) {} 
+
+	//Allow demotion to a lower order polynomial if the highest coefficient is zero
+	PolynomialFunction<Order-1> lowerOrder() const
+	{
+#ifdef MAGNET_DEBUG
+	  if ((*this)[Order] != 0) M_throw() << "Trying to reduce the order of a polynomial with non-zero highest order coefficients!";
+#endif
+	  PolynomialFunction<Order-1> f;
+	  for (size_t i(0); i < Order; ++i)
+	    f[i] = (*this)[i];
+	  return f;
+	}
+	
+	template<size_t first_deriv=0, size_t nderivs=1>
+	std::array<double, nderivs> eval(double dt) const {
+	  std::array<double, nderivs> retval;
+	  for (size_t j(0); j < nderivs; ++j)
+	    {
+	      double accum(0);
+	      for (size_t i(Order); i > first_deriv + j; --i) {
+		accum = (*this)[i] + (dt * accum) / (i + 1 - first_deriv - j);
+	      }
+	      retval[j] = accum * dt + (*this)[first_deriv + j];
+	    }
+	  return retval;
+	}
+	
+	void flipSign() {
+	  for (size_t i(0); i <= Order; ++i)
+	    (*this)[i] = -(*this)[i];
+	}
+
+	double nextEvent() const {
+	  return nextEvent(*this);
+	}
+
+	double order() const { return Order; }
+      };
+
+      inline double nextEvent(const PolynomialFunction<1>& f) {
+	if (f[1] >= 0) return HUGE_VAL;
+	return std::max(0.0, - f[0] / f[1]);
+      }
+
+      inline double nextEvent(const PolynomialFunction<2>& f) {
+	if (f[2] == 0) return nextEvent(f.lowerOrder());
+	const double arg = f[1] * f[1] - 2 * f[2] * f[0];
+	if (f[2] < 0) {
+	  if (arg <= 0) return std::max(0.0, -f[1] / f[2]);
+	  if (f[1] > 0)
+	    return std::max(0.0, (-f[1] - std::sqrt(arg)) / f[2]);
+	  else
+	    return std::max(0.0, 2 * f[0] / (-f[1] + std::sqrt(arg)));
+	} else /*(f[2] > 0)*/ {
+	  const double arg = f[1] * f[1] - 2 * f[2] * f[0];
+	  if ((f[1] >= 0) || arg <= 0) return HUGE_VAL;
+	  return std::max(0.0, 2 * f[0] / (-f[1] + std::sqrt(arg)));
+	}
+      }
+
+      inline double nextEvent(const PolynomialFunction<3>& f)
+      {
 	if (f[3] == 0) return nextEvent(f.lowerOrder());
 
 	//Calculate and sort the roots of the overlap function
-	auto roots = solve_roots(f);
-	std::sort(roots.begin(), roots.end());
+	std::array<double, 3> roots;
+	const size_t nroots = magnet::math::cubicSolve(6 * f[2] / (2 * f[3]), 6 * f[1] / f[3], 6 * f[0] / f[3], roots[0], roots[1], roots[2]);
+	std::sort(roots.begin(), roots.begin() + nroots);
 
 	//Calculate and sort the roots of the overlap function's
 	//derivative
-	auto derivroots = solve_roots(derivative(f));
-	std::sort(derivroots.begin(), derivroots.end());
+	std::array<double, 2> derivroots;
+	const size_t nderivroots = magnet::math::quadSolve(f[3], f[2], f[1], derivroots[0], derivroots[1]);
+	if (derivroots[1] < derivroots[0])
+	  std::swap(derivroots[0], derivroots[1]);
 	
 	if (f[3] > 0) {
-	  //Cubic limits away from overlap at t -> +inf.
-
-	  //The only region where a cubic overlap function with
-	  //positive f[3] is decreasing is between the two turning
-	  //points (if they exist).
-	  
-	  //Check there are two turning points, and that we are before
-	  //the second one.
-	  if (derivroots.size() != 2) return HUGE_VAL;
+	  if (nderivroots == 0) return HUGE_VAL;
 	  if (derivroots[1] < 0) return HUGE_VAL;
-	  
-	  //If there is only one root, it must come after the first
-	  //turning point (otherwise the two turning points are
-	  //outside the overlapped (negative f) zone.
-	  if ((roots.size()==1) && (roots[0] < derivroots[0])) return HUGE_VAL;
-
-	  //There is definitely an event. 
-
-	  if ((roots.size() == 1) || (roots.size() == 2))
-	    //If there's one root, the event happens after the first
-	    //turning point. If there's two roots, the first turning
-	    //point must be a point of inflection, and it still
-	    //happens after the first turning point.
-	    return std::max(0.0, derivroots[0]);
-	  
-	  //If there are three roots it happens after the first event.
-	  return std::max(0.0, roots[1]);
-	} 
-
-	/* else (f[3] < 0)*/ 
-	//Cubic limits towards overlap at t -> +inf
-	
-	if (derivroots.size() == 0) {
-	  //If there are no turning points, then the function is always
-	  //decreasing, so it must have one root and the collision
-	  //happens after this point.
-	  if (roots.size() != 1)
-	    M_throw() << "Unexpected solution in cubic function";
-	  return std::max(0.0, roots[0]);
+	  if ((nroots==1) && (roots[0] < derivroots[0])) return HUGE_VAL;
+	  return std::max(0.0, (nroots==3) ? roots[1] : derivroots[0]);
+	} else /*(f[3] < 0)*/ {
+	  if (nderivroots == 0) return std::max(0.0, roots[0]);
+	  if ((derivroots[0] > 0) && (roots[0] < derivroots[0]))
+	    return std::max(0.0, roots[0]);
+	  return std::max(0.0, std::max(derivroots[1], roots[nroots-1]));
 	}
-	
-	//There must be one or more turning points. 
-	if ((derivroots[0] > 0) && (roots[0] < derivroots[0]))
-	  return std::max(0.0, roots[0]);
-	return std::max(0.0, std::max(derivroots[1], roots[nroots-1]));
       }
 
       inline double nextEvent(const PolynomialFunction<4>& f, double f0char, double precision=1e-16)
