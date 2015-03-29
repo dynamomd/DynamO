69874dd2b84dfc19aa7b050f4f712b922fd63dc6
Marcus Bannerman <m.bannerman@gmail.com>
2015-03-29 20:23:37 +0100

    Added a comprehensive replica exchange unit test, including dynahist_rw

--- src/dynamo/tests/replex_test.py
+++ src/dynamo/tests/replex_test.py
@@ -21,12 +21,15 @@ import sys
 import getopt
 import xml.etree.ElementTree as ET
 
-swaps=1000
-swap_time=1
+e_swaps=2500
+p_swaps=25000
+swap_time=0.5
+run=True
 
 #This may not exactly correspond to the real finish time, it may be +-
 #swaptime
-finish_time=float(swaps)*swap_time 
+e_finish_time=float(e_swaps)*swap_time 
+p_finish_time=float(p_swaps)*swap_time 
 
 def isclose(a,b,tol):
     return (abs(a-b) <= abs(tol*a)) or (abs(a-b) <= abs(tol*b))
@@ -64,18 +67,23 @@ Temperatures=[1.0, 2.0, 5.0, 10.0]
 for i,T in enumerate(Temperatures):
     cmd=[dynamod_cmd, "-m2", "-T"+str(T), "-oc"+str(i)+".xml"]
     print " ".join(cmd)
-    subprocess.call(cmd)
+    if run:
+        subprocess.call(cmd)
 
 ###### EQUILIBRATION
-cmd=[dynarun_cmd, "--engine=2", "-oc%ID.xml", "--out-data-file=o%ID.xml", "-N4", "-i"+str(swap_time), "-f"+str(finish_time)]+["c"+str(i)+".xml" for i in range(len(Temperatures))]
+cmd=[dynarun_cmd, "--engine=2", "-oc%ID.xml", "--out-data-file=o%ID.xml", "-N4", "-i"+str(swap_time), "-f"+str(e_finish_time)]+["c"+str(i)+".xml" for i in range(len(Temperatures))]
 print " ".join(cmd)
-subprocess.call(cmd)
+if run:
+    subprocess.call(cmd)
 
 
 ###### PRODUCTION
-cmd=[dynarun_cmd, "--engine=2", "-oc%ID.xml", "--out-data-file=o%ID.xml", "-N4", "-i"+str(swap_time), "-LIntEnergyHist", "-f"+str(finish_time)]+["c"+str(i)+".xml" for i in range(len(Temperatures))]
+cmd=[dynarun_cmd, "--engine=2", "-oc%ID.xml", "--out-data-file=o%ID.xml", "-N4", "-i"+str(swap_time), "-LIntEnergyHist", "-f"+str(p_finish_time)]+["c"+str(i)+".xml" for i in range(len(Temperatures))]
 print " ".join(cmd)
-subprocess.call(cmd)
+if run:
+    subprocess.call(cmd)
+
+###### OUTPUT VALIDATION
 
 import re
 replex_calls=None
@@ -89,8 +97,10 @@ with open("replex.stats") as f:
 if replex_calls is None:
     raise RuntimeError("Could not parse the number of replex calls performed")
 
-if abs(replex_calls - swaps) > 1:
-    raise RuntimeError("The number of actual swaps ("+str(replex_calls)+") is different to the number of requested swaps +("+str(swaps)+")")
+if abs(replex_calls - p_swaps) > 1:
+    raise RuntimeError("The number of actual swaps ("+str(replex_calls)+") is different to the number of requested swaps +("+str(p_swaps)+")")
+
+expectedCvs={10.0: 0.304967, 5.0: 2.52568, 2.0: 20.5657, 1.0: 23.1892}
 
 for i,T in enumerate(Temperatures):
     outputfile="o"+str(i)+".xml"
@@ -101,8 +111,54 @@ for i,T in enumerate(Temperatures):
         raise RuntimeError("Simulation Temperature is different to what is expected:"+str(measured_T)+"!="+str(T))
     
     simtime=float(xmldoc.getroot().find(".//Duration").attrib["Time"])
-    print math.sqrt(min(Temperatures)/T)
     expected_simtime = swap_time * replex_calls * math.sqrt(min(Temperatures)/T)
     if not isclose(simtime, expected_simtime, 1e-4):
         raise RuntimeError("Simulation duration is different to what is expected:"+str(simtime)+"!="+str(expected_simtime))
-        
+
+    measured_Cv=float(xmldoc.getroot().find(".//ResidualHeatCapacity").attrib["Value"])
+    expected_Cv=expectedCvs[T]
+
+    if not isclose(measured_Cv, expected_Cv, 5e-2):
+        raise RuntimeError("Simulation heat capacity is different to what is expected:"+str(measured_Cv)+"!="+str(expected_Cv))
+
+###### dynahist_rw VALIDATION
+cmd=[dynahist_rw_cmd]+["o"+str(i)+".xml" for i in range(len(Temperatures))]
+print " ".join(cmd)
+if run:
+    subprocess.call(cmd)
+
+for line,ref in zip(open('logZ.out', 'r'), [0, -37.1325363028306097, -50.8797611158942076, -53.720182897102171]):
+    logZval = float(line.split()[1])
+    if ref == 0:
+        if logZval != 0:
+            raise RuntimeError("First logZ value is not zero")
+    else:
+        if not isclose(logZval, ref, 1e-2):
+            raise RuntimeError("Calculated logZ value is incorrect:"+str(logZval)+"!="+str(ref))
+
+
+def interpolate(yin, xin, xout):
+  i1 = None
+  for i in range(len(xin)):
+      if xin[i] == xout:
+          return yin[i]
+      if xin[i] < xout:
+          i1 = i
+          break
+  if i1 is None:
+      raise ValueError("xout out of range")
+
+  x0 = xin[i1-1]  
+  x1 = xin[i1]  
+  y0 = yin[i1-1]  
+  y1 = yin[i1] 
+  
+  return (xout - x0) / (x1 - x0) * (y1 - y0) + y0  
+
+Cvdata=[map(float, line.split()) for line in open("Cv.out")]
+
+for T in expectedCvs:
+    measuredCv = interpolate([data[1] for data in Cvdata],  [data[0] for data in Cvdata], T)
+    expectedCv = expectedCvs[T]
+    if not isclose(measuredCv, expectedCv, 1e-1):
+        raise RuntimeError("Histogram reweighted Cv value is incorrect:"+str(measuredCv)+"!="+str(expectedCv))
