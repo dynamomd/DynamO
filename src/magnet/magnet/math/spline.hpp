83a7002694a88384aa275ce0a181647089c8c798
Marcus Bannerman <m.bannerman@gmail.com>
2017-11-15 08:48:58 +0000

    Fix for a boost issue with make_array being undefined

--- src/magnet/magnet/math/spline.hpp
+++ src/magnet/magnet/math/spline.hpp
@@ -17,7 +17,7 @@
 
 #pragma once
 
-#include <boost/serialization/array.hpp>
+#include <boost/serialization/valarray.hpp>
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/numeric/ublas/vector_proxy.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
