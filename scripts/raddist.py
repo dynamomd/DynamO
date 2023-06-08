61ce3ebade1c03193f22211f12baedce7f2d55ae
Marcus Bannerman <m.bannerman@gmail.com>
2023-06-08 15:27:53 +0100

    More wokr

--- scripts/raddist.py
+++ scripts/raddist.py
@@ -7,5 +7,12 @@ output_file = st.file_uploader("Choose a Output file")
 
 if output_file is not None:
     of = pydynamo.OutputFile(output_file)
-    of.tree.findall('.//RadialDistribution/Species')
-    print(of)
+    grs = of.tree.findall('.//RadialDistributionMoments/Species')
+    all_species = set(s.attrib["Name1"] for s in grs).union(set(s.attrib["Name2"] for s in grs))
+    print(all_species)
+
+    for species in grs:
+        for moment in species.findall('./Moment'):
+            for line in moment.text.split("\n")[:3]:
+                print([(species.attrib["Name1"], species.attrib["Name2"], moment.attrib['Order'], *line.split())])
+    print("Done!")
