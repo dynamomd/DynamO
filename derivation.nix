51d6371aa4a82a53d88058080c15f317e67ad321
Marcus Bannerman <m.bannerman@gmail.com>
2025-05-13 12:34:01 +0100

    Got a build and install working, not sure about python packaging yet

---
+++ derivation.nix
@@ -0,0 +1,46 @@
+# see https://unix.stackexchange.com/questions/717168/how-to-package-my-software-in-nix-or-write-my-own-package-derivation-for-nixpkgs
+{ pkgs, python3 }:
+python3.pkgs.buildPythonPackage rec {
+  name = "dynamomd";
+  src = ./.;
+  pyproject = true;
+
+  dontUseCmakeConfigure = true;
+  
+  propagatedBuildInputs = with python3.pkgs; [
+      scikit-build-core
+      numpy
+      alive-progress
+      uncertainties
+      pandas
+      scipy
+      freud
+      networkx
+      pytest
+  ];
+
+  nativeBuildInputs = with pkgs; [
+    cmake
+    ninja
+    git
+    gcc
+    pkg-config
+    clang-tools
+    wrapGAppsHook3
+  ]  ++ propagatedBuildInputs;
+  
+  buildInputs = with pkgs; [
+    # Basic build dependencies
+    bzip2.dev
+    boost.dev
+    eigen
+    
+    # Visualiser
+    gtkmm3.dev
+    ffmpeg.dev
+    freeglut
+    glew
+    cairomm.dev
+    libpng
+  ];
+}
